f95b1d079909197ee6d1e066a794f104ffad1646
Patricia Nykaza <pnykaza@calstatela.edu>
2018-08-21 17:58:28 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,3 +10,4 @@ end
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+project.css("")
